0173eb8f8424108cae2deea72345e26e6d52e7fd
Tanmay Upadhyay <tanmay.upadhyay99@hotmail.com>
2020-05-27 12:45:03 +0530

    Added better plots for Univariate Analysis

--- d_exploration.r
+++ d_exploration.r
@@ -53,7 +53,7 @@ ggplot(dataset, aes(x=SalePrice, y=TotalBsmtSF)) +
 
 
 
-M <- cor(dataset)
+#M <- cor(dataset)
 M <- dataset %>% mutate_if(is.character, as.factor)
 M <- M %>% mutate_if(is.factor, as.numeric)
 M <- cor(M)
@@ -101,92 +101,98 @@ pairs(newData[1:7],
       col="blue"
 )
 
-plotmatrix(newData)
 
-numeric_NA <- dataset[,!grepl("^Bsmt",names(dataset))]      #remove BSMTx variables
 
-numeric_NA <- subset(numeric_NA, select = -c(numeric_NA$PoolQC, numeric_NA$PoolArea, numeric_NA$FullBath, numeric_NA$HalfBath) )
-#numeric_NA = select(numeric_NA, -c(numeric_NA$PoolQC, numeric_A$FullBath, numeric_NA$HalfBath))
+clean_data <- dataset[,!grepl("^Bsmt",names(dataset))]      #remove BSMTx variables
 
-str(numeric_NA)
+
+drops <- c("clean_data$PoolQC", "clean_data$PoolArea", "clean_data$FullBath", "clean_data$HalfBath")
+clean_data <- clean_data[ , !(names(clean_data) %in% drops)]
+
+#clean_data <- subset(clean_data, select = -c(clean_data$PoolQC, clean_data$PoolArea, clean_data$FullBath, clean_data$HalfBath) )
+
+str(clean_data)
+
 
 #Univariate Analysis
 
-numeric_NA$price_norm <- scale(numeric_NA$SalePrice)    #normalizing the price variable
-plot(numeric_NA$price_norm)                             #we can see the outliers clearly 
+clean_data$price_norm <- scale(clean_data$SalePrice)    #normalizing the price variable
+plot(clean_data$price_norm)                             #we can see the outliers clearly 
+
+clean_data$price_norm <- scale(clean_data$SalePrice)
 
 #Bi-variate Analysis
 
 plot(numeric$GrLivArea, numeric$SalePrice, xlim=c(1,5000), main =" General Living Area vs. Sale Price",xlab="Living Area", ylab="Sale Price")
 
 
-#numeric_NA$GrLivArea <- sort(numeric_NA$GrLivArea, decreasing = TRUE)   
-numeric_NA <- numeric_NA[!(numeric_NA$GrLivArea > 4000),]   #remove outliers
+#clean_data$GrLivArea <- sort(clean_data$GrLivArea, decreasing = TRUE)   
+clean_data <- clean_data[!(clean_data$GrLivArea > 4000),]   #remove outliers
 
-smoothScatter(numeric_NA$GrLivArea, numeric_NA$SalePrice,xlim=c(1,5000),  main =" General Living Area vs. Sale Price",xlab="Living Area", ylab="Sale Price")
+smoothScatter(clean_data$GrLivArea, clean_data$SalePrice,xlim=c(1,5000),  main =" General Living Area vs. Sale Price",xlab="Living Area", ylab="Sale Price")
 
-smoothScatter(numeric_NA$SalePrice, numeric_NA$TotalBsmtSF, main =" Total Basement Area vs. Sale Price",xlab="Price", ylab="Basement Area") #The outliers isnt that bad and we can leave them alone.
+smoothScatter(clean_data$SalePrice, clean_data$TotalBsmtSF, main =" Total Basement Area vs. Sale Price",xlab="Price", ylab="Basement Area") #The outliers isnt that bad and we can leave them alone.
 
 
 #advanced
 
-hist(numeric_NA$SalePrice, probability = TRUE, main = "Sales Price Density", xlab = "Price")
-lines(density(numeric_NA$SalePrice))
-lines(density(numeric_NA$SalePrice, adjust=5),col="red")
+hist(clean_data$SalePrice, probability = TRUE, main = "Sales Price Density", xlab = "Price")
+lines(density(clean_data$SalePrice))
+lines(density(clean_data$SalePrice, adjust=5),col="red")
 
-p <- probplot(numeric_NA$SalePrice, line=FALSE)
+p <- probplot(clean_data$SalePrice, line=FALSE)
 lines(p, col="red", lty=2, lwd=2)
 
-numeric_NA$log_price <- log(numeric_NA$SalePrice)         #we use log transformation to convert values
+clean_data$log_price <- log(clean_data$SalePrice)         #we use log transformation to convert values
 
-hist(numeric_NA$log_price, probability = TRUE, main = "Sales Price Density", xlab = "Price", xlim = c(10.0,14.0))
-lines(density(numeric_NA$log_price))
-lines(density(numeric_NA$log_price, adjust=5),col="red")
+hist(clean_data$log_price, probability = TRUE, main = "Sales Price Density", xlab = "Price", xlim = c(10.0,14.0))
+lines(density(clean_data$log_price))
+lines(density(clean_data$log_price, adjust=5),col="red")
 
-p <- probplot(numeric_NA$log_price, line=FALSE)           
-lines(p, col="red", lty=2, lwd=2)
-
-
-
-
-hist(numeric_NA$GrLivArea, probability = TRUE, main = "General Living Area Density", xlab = "Area")
-lines(density(numeric_NA$GrLivArea))
-lines(density(numeric_NA$GrLivArea, adjust=5),col="red") 
-
-p <- probplot(numeric_NA$GrLivArea, line=FALSE)
-lines(p, col="red", lty=2, lwd=2)
-
-numeric_NA$grlive_log <- log(numeric_NA$GrLivArea) #log transformation
-
-hist(numeric_NA$grlive_log, probability = TRUE, main = "General Living Area Density (log) ", xlab = "Area", xlim = c(5.5,8.5))
-lines(density(numeric_NA$grlive_log))
-lines(density(numeric_NA$grlive_log, adjust=5),col="red") 
-
-p <- probplot(numeric_NA$grlive_log, line=FALSE)
+p <- probplot(clean_data$log_price, line=FALSE)           
 lines(p, col="red", lty=2, lwd=2)
 
 
 
 
+hist(clean_data$GrLivArea, probability = TRUE, main = "General Living Area Density", xlab = "Area")
+lines(density(clean_data$GrLivArea))
+lines(density(clean_data$GrLivArea, adjust=5),col="red") 
 
-hist(numeric_NA$TotalBsmtSF, probability = TRUE, main = "Total Basement Area Density", xlab = "Area")
-lines(density(numeric_NA$TotalBsmtSF))
-lines(density(numeric_NA$TotalBsmtSF, adjust=5),col="red") 
-
-p <- probplot(numeric_NA$TotalBsmtSF, line=FALSE)
+p <- probplot(clean_data$GrLivArea, line=FALSE)
 lines(p, col="red", lty=2, lwd=2)
 
-numeric_NA <- transform(numeric_NA, cat_bsmt = ifelse(TotalBsmtSF>0, 1, 0))
+clean_data$grlive_log <- log(clean_data$GrLivArea) #log transformation
 
-numeric_NA$totalbsmt_log <- log(numeric_NA$TotalBsmtSF) #log transformation
+hist(clean_data$grlive_log, probability = TRUE, main = "General Living Area Density (log) ", xlab = "Area", xlim = c(5.5,8.5))
+lines(density(clean_data$grlive_log))
+lines(density(clean_data$grlive_log, adjust=5),col="red") 
 
-numeric_NA<-transform(numeric_NA,totalbsmt_log = ifelse(cat_bsmt == 1, log(TotalBsmtSF), 0 ))
+p <- probplot(clean_data$grlive_log, line=FALSE)
+lines(p, col="red", lty=2, lwd=2)
 
-hist(numeric_NA$totalbsmt_log, probability = TRUE, main = "Total Basement Area Density (log) ", xlab = "Area", xlim = c(4.5,8.5))
-lines(density(numeric_NA$totalbsmt_log))
-lines(density(numeric_NA$totalbsmt_log, adjust=5),col="red") 
 
-p <- probplot(numeric_NA$totalbsmt_log, line=FALSE)
+
+
+
+hist(clean_data$TotalBsmtSF, probability = TRUE, main = "Total Basement Area Density", xlab = "Area")
+lines(density(clean_data$TotalBsmtSF))
+lines(density(clean_data$TotalBsmtSF, adjust=5),col="red") 
+
+p <- probplot(clean_data$TotalBsmtSF, line=FALSE)
+lines(p, col="red", lty=2, lwd=2)
+
+clean_data <- transform(clean_data, cat_bsmt = ifelse(TotalBsmtSF>0, 1, 0))
+
+clean_data$totalbsmt_log <- log(clean_data$TotalBsmtSF) #log transformation
+
+clean_data<-transform(clean_data,totalbsmt_log = ifelse(cat_bsmt == 1, log(TotalBsmtSF), 0 ))
+
+hist(clean_data$totalbsmt_log, probability = TRUE, main = "Total Basement Area Density (log) ", xlab = "Area", xlim = c(4.5,8.5))
+lines(density(clean_data$totalbsmt_log))
+lines(density(clean_data$totalbsmt_log, adjust=5),col="red") 
+
+p <- probplot(clean_data$totalbsmt_log, line=FALSE)
 lines(p, col="red", lty=2, lwd=2)
 
 
@@ -194,8 +200,8 @@ lines(p, col="red", lty=2, lwd=2)
 
 #checking for homoscedasiticity
 
-plot(numeric_NA$grlive_log, numeric_NA$log_price, main =" Homoscedasticity for Living Area vs. Sale Price",xlab="Living Area", ylab="price")
+plot(clean_data$grlive_log, clean_data$log_price, main =" Homoscedasticity for Living Area vs. Sale Price",xlab="Living Area", ylab="price")
 
-plot(numeric_NA$totalbsmt_log, numeric_NA$log_price,  main =" Homoscedasticity for Total Basement Area vs. Sale Price",xlab="Basement Area", ylab="price") 
+plot(clean_data$totalbsmt_log, clean_data$log_price,  main =" Homoscedasticity for Total Basement Area vs. Sale Price",xlab="Basement Area", ylab="price") 
 
 
